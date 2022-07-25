719283cef30c03d944b9a68123c5100f61b0d3c0
Sedrak Qocharyan <sedrakqocharyan@gmail.com>
2022-07-25 21:44:47 +0400

    Your awesome message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import Model from "./components/Model.jsx";
+
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => setOpen(false);
+
+  function handleToggle() {
+   setOpen(!open)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleToggle}>add modal</button>
+
+      <Model open={open} handleClose={handleClose}></Model>
     </div>
   );
 }
 
 export default App;
+
+// import React, { useState } from "react";
+
+// function App() {
+//   const [showModal, setShow] = useState(false);
+
+//   const handleClose = () => setShow(false);
+//   const handleShow = () => setShow(true);
+
+//   return (
+//     <div>
+//       <div
+//         className="d-flex align-items-center justify-content-center"
+//         style={{ height: "100vh" }}
+//       >
+//         <button variant="primary" onClick={handleShow}>
+//           Launch demo modal
+//         </button>
+//       </div>
+//       <div show={showModal} onHide={handleClose}>
+//         <div>
+//           <button variant="secondary" onClick={handleClose}>
+//             Close
+//           </button>
+//           <button variant="primary" onClick={handleClose}>
+//             Save Changes
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// // export default App;
---
+++ src/components/Model.jsx
@@ -0,0 +1,16 @@
+function Model({ open, handleClose }) {
+  if (open === false) {
+    return null;
+  }
+
+  return (
+    <div className="box">
+      <div className="Model">
+        <h2>Your content</h2>
+        <button onClick={handleClose}> X </button>
+      </div>
+    </div>
+  );
+}
+
+export default Model;
